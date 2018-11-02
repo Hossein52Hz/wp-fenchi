f18b79d4695155e4fd716f8bb5f4c7b021c1b5f4
csmasoudi <csmasoudi@gmail.com>
2018-11-02 23:50:02 +0330

    create sidebar section

--- sidebar.php
+++ sidebar.php
@@ -6,12 +6,338 @@
  *
  * @package fenchi
  */
-
-if ( ! is_active_sidebar( 'sidebar-1' ) ) {
-	return;
-}
 ?>
+<!-- sidebar -->
+<div class="col-md-4 sidebar">
+  <!-- About me -->
+  <div class="card my-4 shadow-sm">
+	<img class="img-responsive authorPicture" src="layout/img/pixabay/lightgreen/g0.jpg">
+	<div class="card-body">
+	  <h5 class="lineLeft"><span>Joee Albakry</span></h5>
+	  <h5 class="lineLeft"><span>Programmer</span></h5>
+	  <div class="aboutMe">
+		<p>I want to tell you something. The world will not messy when you spend your time for other things
+		  instead doing “coding”. You are not the only programmer in this world. The fact is you are a human
+		  that have a life. You have your own life. We indeed have to responsible for our job. But responsible
+		  doesn’t mean we have to work 24 hours, or even more.
+		  So, now it’s your time to spend your money and say, “I am a programmer, and I have a life” :).</p>
+	  </div>
+	  <div class="socialLink">
 
-<aside id="secondary" class="widget-area">
-	<?php dynamic_sidebar( 'sidebar-1' ); ?>
-</aside><!-- #secondary -->
+		<a href="#"><i class="fab fa-facebook-f"></i></a>
+		<a href="#"><i class="fab fa-stack-overflow"></i></a>
+		<a href="#"><i class="fab fa-gitlab"></i></a>
+		<a href="#"><i class="fab fa-twitter"></i></a>
+		<a href="#"><i class="fab fa-linkedin-in"></i></a>
+		<a href="#"><i class="fab fa-pinterest-p"></i></a>
+		<a href="#"><i class="far fa-envelope"></i></a>
+		<a href="#"><i class="fas fa-phone"></i></a>
+		<a href="#"><i class="fab fa-google-plus-g"></i></a>
+
+
+	  </div>
+	</div>
+  </div>
+  <!--end About me -->
+  <!-- search box -->
+  <div class="card my-4 shadow-sm">
+	<div class="card-body">
+	  <div class="input-group">
+		<input type="text" class="form-control" placeholder="Search here">
+		<div class="input-group-append">
+		  <button class="btn btn-secondary doSearch" type="button">
+			<i class="fa fa-search"></i>
+		  </button>
+		</div>
+	  </div>
+	</div>
+  </div>
+  <!-- end search box -->
+
+  <!--  category posts -->
+  <div class="card my-4 shadow-sm">
+	<div class="card-body">
+	  <h2 class="widgetTitle">Categories</h2>
+	  <ul class="groupItem">
+		<li><a href="#"><i class="fas fa-caret-right"></i>News</a></li>
+		<li><a href="#"><i class="fas fa-caret-right"></i>Programming</a></li>
+		<li><a href="#"><i class="fas fa-caret-right"></i>Data Base</a></li>
+		<li><a href="#"><i class="fas fa-caret-right"></i>Short Story</a></li>
+		<li><a href="#"><i class="fas fa-caret-right"></i>Leisure times</a></li>
+		<li><a href="#"><i class="fas fa-caret-right"></i>Tips & Trick</a></li>
+		<li><a href="#"><i class="fas fa-caret-right"></i>Design web</a></li>
+		<li><a href="#"><i class="fas fa-caret-right"></i>Data structure</a></li>
+		<li><a href="#"><i class="fas fa-caret-right"></i>Gaming</a></li>
+		<li><a href="#"><i class="fas fa-caret-right"></i>Workshops</a></li>
+	  </ul>
+	</div>
+  </div>
+  <!-- end category posts -->
+
+  <!--  Latest post with img -->
+  <div class="card my-4 shadow-sm">
+	<div class="card-body">
+	  <h2 class="widgetTitle"><span>Latest Posts</span></h2>
+	  <ul class="groupItem">
+		<li class="latestPostTitle">
+		  <a href="#">
+			<img class="postImg64" src="layout/img/pixabay/lightgreen/g1.jpg" alt="">
+			google summer of code 2018 final oogle summer of code 2018 final report
+			<br>
+			23 comments | 230 views
+		  </a>
+
+		</li>
+		<li class="latestPostTitle">
+		  <a href="#">
+			<img class="postImg64" src="layout/img/pixabay/lightgreen/g3.jpg" alt="">
+			google summer of code 2018 final oogle summer of code 2018 final report
+			<br>
+			23 comments | 230 views
+		  </a>
+
+		</li>
+
+		<li class="latestPostTitle">
+		  <a href="#">
+			<img class="postImg64" src="layout/img/pixabay/lightgreen/g4.jpg" alt="">
+			google summer of code 2018 final oogle summer of code 2018 final report
+			<br>
+			23 comments | 230 views
+		  </a>
+
+		</li>
+
+		<li class="latestPostTitle">
+		  <a href="#">
+			<img class="postImg64" src="layout/img/pixabay/lightgreen/g7.jpg" alt="">
+			google summer of code 2018 final oogle summer of code 2018 final report
+			<br>
+			23 comments | 230 views
+		  </a>
+
+		</li>
+		<li class="latestPostTitle">
+		  <a href="#">
+			<img class="postImg64" src="layout/img/pixabay/lightgreen/g12.jpg" alt="">
+			google summer of code 2018 final oogle summer of code 2018 final report
+			<br>
+			23 comments | 230 views
+		  </a>
+
+		</li>
+		<li class="latestPostTitle">
+		  <a href="#">
+			<img class="postImg64" src="layout/img/pixabay/lightgreen/g18.jpg" alt="">
+			google summer of code 2018 final oogle summer of code 2018 final report
+			<br>
+			23 comments | 230 views
+		  </a>
+
+		</li>
+
+		<li class="latestPostTitle">
+		  <a href="#">
+			<img class="postImg64" src="layout/img/pixabay/lightgreen/g19.jpg" alt="">
+			google summer of code 2018 final oogle summer of code 2018 final report
+			<br>
+			23 comments | 230 views
+		  </a>
+
+		</li>
+
+		<li class="latestPostTitle">
+		  <a href="#">
+			<img class="postImg64" src="layout/img/pixabay/lightgreen/g27.jpg" alt="">
+			google summer of code 2018 final oogle summer of code 2018 final report
+			<br>
+			23 comments | 230 views
+		  </a>
+
+		</li>
+		<li class="latestPostTitle">
+		  <a href="#">
+			<img class="postImg64" src="layout/img/pixabay/lightgreen/g22.jpg" alt="">
+			google summer of code 2018 final oogle summer of code 2018 final report
+			<br>
+			23 comments | 230 views
+		  </a>
+
+		</li>
+
+
+
+
+	  </ul>
+	</div>
+  </div>
+  <!-- end Latest post with img -->
+
+
+  <!--  Tags -->
+  <div class="card my-4 shadow-sm">
+	<div class="card-body">
+	  <h2 class="widgetTitle">Tags</h2>
+	  <ul class="tagItem">
+		<li><a href="#">News</a></li>
+		<li><a href="#">Programming</a></li>
+		<li><a href="#">Data Base</a></li>
+		<li><a href="#">Short Story</a></li>
+		<li><a href="#">Leisure times</a></li>
+		<li><a href="#">Tips & Trick</a></li>
+		<li><a href="#">Design web</a></li>
+		<li><a href="#">Data structure</a></li>
+		<li><a href="#">Gaming</a></li>
+		<li><a href="#">Workshops</a></li>
+	  </ul>
+	</div>
+  </div>
+  <!-- end Tags -->
+
+  <!--  banner -->
+  <div class="card my-4 shadow-sm">
+	<div class="card-body">
+	  <h2 class="widgetTitle"><span>Banner picture</span></h2>
+	  <a href="#">
+	  <img class="bannerImg" src="layout/img/pixabay/lightgreen/g1.jpg" alt="">
+	</a>
+	</div>
+  </div>
+  <!-- end banner -->
+   <!--  banner -->
+   <div class="card my-4 shadow-sm">
+	<div class="card-body">
+	  <h2 class="widgetTitle"><span>Banner picture</span></h2>
+	  <a href="#">
+	  <img class="bannerImg" src="layout/img/pixabay/lightgreen/g29.jpg" alt="">
+	</a>
+	</div>
+  </div>
+  <!-- end banner -->
+
+  <!--  Latest posts -->
+  <div class="card my-4 shadow-sm">
+	<div class="card-body">
+	  <h2 class="widgetTitle">Latest posts</h2>
+	  <ul class="groupItem">
+
+		<li><a href="#"><i class="fas fa-caret-right"></i>google summer of code 2018 final report</a>
+		  <a href="#"><span class="views">23 comments | 230 views</span></a>
+		</li>
+
+		<li><a href="#"><i class="fas fa-caret-right"></i>google summer of code 2018 final report</a>
+		  <a href="#"><span class="views">23 comments | 230 views</span></a>
+		</li>
+
+		<li><a href="#"><i class="fas fa-caret-right"></i>google summer of code 2018 final report</a>
+		  <a href="#"><span class="views">23 comments | 230 views</span></a>
+		</li>
+
+		<li><a href="#"><i class="fas fa-caret-right"></i>google summer of code 2018 final report</a>
+		  <a href="#"><span class="views">23 comments | 230 views</span></a>
+		</li>
+		<li><a href="#"><i class="fas fa-caret-right"></i>google summer of code 2018 final report</a>
+		  <a href="#"><span class="views">23 comments | 230 views</span></a>
+		</li>
+		<li><a href="#"><i class="fas fa-caret-right"></i>google summer of code 2018 final report</a>
+		  <a href="#"><span class="views">23 comments | 230 views</span></a>
+		</li>
+		<li><a href="#"><i class="fas fa-caret-right"></i>google summer of code 2018 final report</a>
+		  <a href="#"><span class="views">23 comments | 230 views</span></a>
+		</li>
+		<li><a href="#"><i class="fas fa-caret-right"></i>google summer of code 2018 final report</a>
+		  <a href="#"><span class="views">23 comments | 230 views</span></a>
+		</li>
+		<li><a href="#"><i class="fas fa-caret-right"></i>google summer of code 2018 final report</a>
+		  <a href="#"><span class="views">23 comments | 230 views</span></a>
+		</li>
+		<li><a href="#"><i class="fas fa-caret-right"></i>google summer of code 2018 final report</a>
+		  <a href="#"><span class="views">23 comments | 230 views</span></a>
+		</li>
+	  </ul>
+	</div>
+  </div>
+  <!-- end  Latest posts -->
+  <!--  Latest Commnets -->
+  <div class="card my-4 shadow-sm">
+	<div class="card-body">
+	  <h2 class="widgetTitle"><span>Latest Commnets</span></h2>
+	  <ul class="groupItem">
+		<li class="latestComments">
+		  <a href="#">
+			<img class="userAvatar" src="layout/img/pixabay/profile/p1.jpg" alt="">
+			<p class="userName">Joee:</p>
+			<p class="comment">google summer of code 2018 final oogle summer of code 2018 final report</p>
+		  </a>
+		</li>
+		<li class="latestComments">
+		  <a href="#">
+			<img class="userAvatar" src="layout/img/pixabay/profile/p2.jpg" alt="">
+			<p class="userName">Joee:</p>
+			<p class="comment">google summer of code 2018 final oogle summer of code 2018 final report</p>
+		  </a>
+		</li>
+		<li class="latestComments">
+		  <a href="#">
+			<img class="userAvatar" src="layout/img/pixabay/profile/p3.jpg" alt="">
+			<p class="userName">Joee:</p>
+			<p class="comment">google summer of code 2018 final oogle summer of code 2018 final report</p>
+		  </a>
+		</li>
+		<li class="latestComments">
+		  <a href="#">
+			<img class="userAvatar" src="layout/img/pixabay/profile/p4.jpg" alt="">
+			<p class="userName">Joee:</p>
+			<p class="comment">google summer of code 2018 final oogle summer of code 2018 final report</p>
+		  </a>
+		</li>
+		<li class="latestComments">
+		  <a href="#">
+
+			<img class="userAvatar" src="layout/img/pixabay/profile/p5.jpg" alt="">
+			<p class="userName">Joee:</p>
+			<p class="comment">google summer of code 2018 final oogle summer of code 2018 final report</p>
+		  </a>
+		</li>
+		<li class="latestComments">
+		  <a href="#">
+
+			<img class="userAvatar" src="layout/img/pixabay/profile/p6.jpg" alt="">
+			<p class="userName">Joee:</p>
+			<p class="comment">google summer of code 2018 final oogle summer of code 2018 final report</p>
+		  </a>
+		</li>
+		<li class="latestComments">
+		  <a href="#">
+			<img class="userAvatar" src="layout/img/pixabay/profile/p1.jpg" alt="">
+			<p class="userName">Joee:</p>
+			<p class="comment">google summer of code 2018 final oogle summer of code 2018 final report</p>
+		  </a>
+		</li>
+		<li class="latestComments">
+		  <a href="#">
+			<img class="userAvatar" src="layout/img/pixabay/profile/p2.jpg" alt="">
+			<p class="userName">Joee:</p>
+			<p class="comment">google summer of code 2018 final oogle summer of code 2018 final report</p>
+		  </a>
+		</li>
+		<li class="latestComments">
+		  <a href="#">
+			<img class="userAvatar" src="layout/img/pixabay/profile/p3.jpg" alt="">
+			<p class="userName">Joee:</p>
+			<p class="comment">google summer of code 2018 final oogle summer of code 2018 final report</p>
+		  </a>
+		</li>
+
+
+
+
+	  </ul>
+	</div>
+  </div>
+  <!-- end Latest Commnets -->
+
+</div>
+<!-- end sidebar -->
+
+</div>
