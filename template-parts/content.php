2bcf5c76db992151711fdd7bf40c92b874e143da
csmasoudi <csmasoudi@gmail.com>
2018-11-02 23:50:42 +0330

    create main display content

--- template-parts/content.php
+++ template-parts/content.php
@@ -9,51 +9,51 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php
-		if ( is_singular() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
-		else :
-			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-		endif;
+<!-- post type2 -->
+<div class="card mb-4 shadow-sm">
+                <div class="category">
+                    <img class="thumbnail" src="layout/img/pixabay/lightgreen/g19.jpg" alt="">
+                    <div class="catList">
+                      <ul>
+                        <li><a href="#"> News,</a></li>
+                        <li><a href="#"> programming,</a></li>
+                        <li><a href="#"> sport </a></li>
+                      </ul>
+                    </div>
+                  </div>
+              <div class="card-body">
+                <div class="social">
+                  <h4 class="author"><span><a href="#"> By Martin.bab</a></span></h4>
+                  <h4 class="shareLink"><span>
+                      <a href="#" data-toggle="tooltip" title="Share on Facebook"><i class="fab fa-facebook-square"></i></a>
+                      <a href="#" data-toggle="tooltip" title="Share on Twitter"><i class="fab fa-twitter-square"></i></a>
+                      <a href="#" data-toggle="tooltip" title="Share on Linkedin"><i class="fab fa-linkedin"></i></a>
+                      <a href="#" data-toggle="tooltip" title="Share on Google+"><i class="fab fa-google-plus-square"></i></a>
+                      <a href="#" data-toggle="tooltip" title="Share on Pinterest"><i class="fab fa-pinterest-square"></i></a>
+                      <a href="mailto:?subject=I wanted you to see this site" data-toggle="tooltip" title="Send via Email"><i
+                          class="fas fa-envelope-square"></i></a>
+                    </span></h4>
+                </div>
+                <div class="clear"></div>
+                <div class="postDetails">
 
-		if ( 'post' === get_post_type() ) :
-			?>
-			<div class="entry-meta">
-				<?php
-				fenchi_posted_on();
-				fenchi_posted_by();
-				?>
-			</div><!-- .entry-meta -->
-		<?php endif; ?>
-	</header><!-- .entry-header -->
+                  <a href="post.html">
+                    <h2 class="postTitle">google summer of code 2018 final report: automatic builds with clang using
+                      open build service</h2>
+                  </a>
+                  <a href="post.html"><span class="publishDate">27 August 2018</span></a>
+                </div>
+                <div class="postContent">
+                  <p>If you are on a personal connection, like at home, you can run an anti-virus scan on your device
+                    to make sure it is not infected with malware. If you are at an office or shared network, you can
+                    ask the network administrator to run a scan across the network looking for misconfigured or
+                    infected devices. Another way to prevent getting this page in the future is to use Privacy Pass.
+                    Check out the browser extension in the Chrome Store.</p>
+                  <a href="#"><small class="readMore">Read more...</small></a>
+                </div>
 
-	<?php fenchi_post_thumbnail(); ?>
 
-	<div class="entry-content">
-		<?php
-		the_content( sprintf(
-			wp_kses(
-				/* translators: %s: Name of current post. Only visible to screen readers */
-				__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'fenchi' ),
-				array(
-					'span' => array(
-						'class' => array(),
-					),
-				)
-			),
-			get_the_title()
-		) );
 
-		wp_link_pages( array(
-			'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'fenchi' ),
-			'after'  => '</div>',
-		) );
-		?>
-	</div><!-- .entry-content -->
-
-	<footer class="entry-footer">
-		<?php fenchi_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
-</article><!-- #post-<?php the_ID(); ?> -->
+              </div>
+            </div>
+            <!-- end post type2 -->
