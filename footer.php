1864e2f00356fd3000bf4051b63a5a242adc0440
csmasoudi <csmasoudi@gmail.com>
2018-11-02 23:51:03 +0330

    update footer codes

--- footer.php
+++ footer.php
@@ -10,32 +10,11 @@
  */
 
 ?>
-      </div>
-
-<!-- page navigation -->
-<nav aria-label="...">
-  <ul class="pagination">
-	<li class="page-item disabled">
-	  <a class="page-link" href="#" tabindex="-1">Previous</a>
-	</li>
-	<li class="page-item"><a class="page-link" href="#">1</a></li>
-	<li class="page-item active">
-	  <a class="page-link" href="#">2 <span class="sr-only">(current)</span></a>
-	</li>
-	<li class="page-item"><a class="page-link" href="#">3</a></li>
-	<li class="page-item"><a class="page-link" href="#">4</a></li>
-	<li class="page-item"><a class="page-link" href="#">5</a></li>
-	<li class="page-item">
-	  <a class="page-link" href="#">Next</a>
-	</li>
-  </ul>
-</nav>
-<!-- end page navigation -->
-</div>
-<!-- container -->
-</section>
-<!-- end main-content -->
-<!-- footer -->
+ 
+ </div>
+		<!-- container -->
+		</section>
+		<!-- end main-content -->
 <footer>
 <div class="footer-bottom">
 <div class="container">
