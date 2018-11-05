1778e9b0ca15090bc779eb8bd201e90f694d2456
csmasoudi <csmasoudi@gmail.com>
2018-11-05 14:21:46 +0330

    delete test code of search box

--- sidebar.php
+++ sidebar.php
@@ -8,23 +8,7 @@
  */
 ?>
 <!-- sidebar -->
-<div class="col-md-4 sidebar">
-
-    <!-- search box -->
-    <div class="card my-4 shadow-sm">
-            <div class="card-body">
-              <div class="input-group">
-                <input type="text" class="form-control" placeholder="Search here">
-                <div class="input-group-append">
-                  <button class="btn btn-secondary doSearch" type="button">
-                    <i class="fa fa-search"></i>
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-          <!-- end search box -->
-		  
+<div class="col-md-4 sidebar">	  
 		<?php if ( is_active_sidebar( 'main-sidebar' ) ) : ?>
 				<?php dynamic_sidebar( 'main-sidebar' ); ?>
 		<?php endif; ?>
