64a1f1980bbb7f149dec55635fb4f830c16e4293
csmasoudi <csmasoudi@gmail.com>
2018-11-05 14:22:31 +0330

    make searchform design to use every where

---
+++ searchform.php
@@ -0,0 +1,12 @@
+<form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+<div class="input-group">
+    
+        <input type="search" class="search-field form-control" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'fenchi' ); ?>" value="<?php echo esc_attr( get_search_query() ); ?>" name="s" title="<?php _ex( 'Search for:', 'label', 'fenchi' ); ?>">
+    
+    <div class="input-group-append">
+                  <button class="btn btn-secondary doSearch" type="submit">
+                    <i class="fa fa-search"></i>
+                  </button>
+    </div>
+</div>
+</form>
