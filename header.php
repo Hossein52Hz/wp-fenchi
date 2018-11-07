c128d0d77cf464e7d66be593f18c36e2151ad853
csmasoudi <csmasoudi@gmail.com>
2018-11-07 23:36:49 +0330

    enable change logo and title in nav-bar by customizer

--- header.php
+++ header.php
@@ -56,11 +56,29 @@
     <div class="navbar  bg-white shadow-sm">
       <div class="container d-flex justify-content-between">
         <!-- main navbar -->
-
-        <nav class="navbar navbar-expand-lg">
-            <!-- <a  href="#">LinuxMotto</a> -->
-       
-
+        <div class="site-branding">
+			<?php
+      the_custom_logo();?>
+    <div id="info">
+      <?php
+      
+			if ( is_front_page() && is_home() ) :
+				?>
+				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+				<?php
+			else :
+				?>
+				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+				<?php
+			endif;
+			$fenchi_description = get_bloginfo( 'description', 'display' );
+			if ( $fenchi_description || is_customize_preview() ) :
+				?>
+				<p class="site-description"><?php echo $fenchi_description; /* WPCS: xss ok. */ ?></p>
+      <?php endif; ?>
+      </div>
+		</div><!-- .site-branding -->
+        <nav class="navbar navbar-expand-lg main-menu">
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive"
             aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             <span id="" class="navbar-toggler-icon"><i class="fas fa-bars"></i></span>
