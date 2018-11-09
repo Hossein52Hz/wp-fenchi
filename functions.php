bd05880836ef67e7c0dc219543f644987c6ac857
csmasoudi <csmasoudi@gmail.com>
2018-11-09 10:15:37 +0330

    move cusomizer.php

--- functions.php
+++ functions.php
@@ -223,7 +223,7 @@ require get_template_directory() . '/inc/template-functions.php';
  * Customizer additions.
  */
 // Main customizer
-require get_template_directory() . '/inc/customizer.php';
+require get_template_directory() . '/inc/customize/customizer.php';
 // Site info section
 require get_template_directory() . '/inc/customize/about-site-setting.php';
 // footer section
