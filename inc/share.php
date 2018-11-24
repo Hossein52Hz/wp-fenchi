d897a5e95511d881889cb847dfb58ca723f08111
csmasoudi <csmasoudi@gmail.com>
2018-11-25 01:32:08 +0330

    change togel to class and fix js bug

--- inc/share.php
+++ inc/share.php
@@ -1,11 +1,11 @@
 <div class="col-8 cal-sm-8">
     <h4 class="share-link"><span>
-      <a href="https://www.facebook.com/sharer/sharer.php?u=<?php the_permalink(); ?>" data-toggle="tooltip" title="Share on Facebook"><i class="fab fa-facebook-square"></i></a>
-      <a href="https://twitter.com/home?status=<?php the_title(); ?> <?php the_permalink(); ?>" data-toggle="tooltip" title="Share on Twitter"><i class="fab fa-twitter-square"></i></a>
-      <a href="https://www.linkedin.com/shareArticle?mini=true&amp;url=<?php the_permalink(); ?>&amp;title=<?php the_title(); ?>&amp;summary=<?php the_title(); ?>" data-toggle="tooltip" title="Share on Linkedin"><i class="fab fa-linkedin"></i></a>
-      <a href="https://plus.google.com/share?url=<?php the_permalink(); ?>" data-toggle="tooltip" title="Share on Google+"><i class="fab fa-google-plus-square"></i></a>
-      <a href="https://pinterest.com/pin/create/button/?url=<?php the_permalink(); ?>&amp;media=<?php  ?>&amp;description=<?php the_title(); ?>" data-toggle="tooltip" title="Share on Pinterest"><i class="fab fa-pinterest-square"></i></a>
-      <a href="mailto:?subject=<?php the_title(); ?>&amp;body=<?php the_permalink(); ?>" data-toggle="tooltip" title="Send via Email"><i class="fas fa-envelope-square"></i></a>
+      <a href="https://www.facebook.com/sharer/sharer.php?u=<?php the_permalink(); ?>"  class="bs-tooltip" title="Share on Facebook"><i class="fab fa-facebook-square"></i></a>
+      <a href="https://twitter.com/home?status=<?php the_title(); ?> <?php the_permalink(); ?>" class="bs-tooltip" title="Share on Twitter"><i class="fab fa-twitter-square"></i></a>
+      <a href="https://www.linkedin.com/shareArticle?mini=true&amp;url=<?php the_permalink(); ?>&amp;title=<?php the_title(); ?>&amp;summary=<?php the_title(); ?>" class="bs-tooltip" title="Share on Linkedin"><i class="fab fa-linkedin"></i></a>
+      <a href="https://plus.google.com/share?url=<?php the_permalink(); ?>" class="bs-tooltip" title="Share on Google+"><i class="fab fa-google-plus-square"></i></a>
+      <a href="https://pinterest.com/pin/create/button/?url=<?php the_permalink(); ?>&amp;media=<?php  ?>&amp;description=<?php the_title(); ?>" class="bs-tooltip" title="Share on Pinterest"><i class="fab fa-pinterest-square"></i></a>
+      <a href="mailto:?subject=<?php the_title(); ?>&amp;body=<?php the_permalink(); ?>" class="bs-tooltip" title="Send via Email"><i class="fas fa-envelope-square"></i></a>
       </span>
     </h4>
 </div>
